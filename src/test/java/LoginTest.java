21c3121c94c5ac065fb2c2facc38ca96a130f5a9
amirmammadov <amirmammado@gmail.com>
2026-05-18 20:47:54 +0200

    Implement runtime properties file parsing and runtime data fuzzing

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -5,13 +5,30 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
+import java.util.UUID;
 
 public class LoginTest {
     private WebDriver driver;
+    private String baseUrl;
+    private String emailDomain;
 
     @Before
     public void setup() throws Exception {
+        Properties prop = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                baseUrl = "https://iticket.az/";
+                emailDomain = "@elte.hu";
+            } else {
+                prop.load(input);
+                baseUrl = prop.getProperty("base.url");
+                emailDomain = prop.getProperty("invalid.email.domain");
+            }
+        }
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.addArguments("--disable-popup-blocking");
@@ -23,7 +40,7 @@ public class LoginTest {
 
     @Test
     public void testIticketLoginWithInvalidCredentials() throws Exception {
-        driver.get("https://iticket.az/");
+        driver.get(baseUrl);
 
         String pageTitle = driver.getTitle();
         Assert.assertTrue(pageTitle.contains("iTicket.AZ"));
@@ -41,9 +58,12 @@ public class LoginTest {
         Thread.sleep(1500);
         driver.navigate().refresh(); 
 
+        String randomEmail = "student-" + UUID.randomUUID().toString().substring(0, 8) + emailDomain;
+        String randomPassword = "Pass" + UUID.randomUUID().toString().substring(0, 8) + "!";
+
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickHeaderLogin();
-        loginPage.enterCredentials("teststudent@elte.hu", "WrongPassword123!");
+        loginPage.enterCredentials(randomEmail, randomPassword);
         loginPage.submitLogin();
     }
 
